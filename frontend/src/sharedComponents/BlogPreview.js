92f36c25133a8d059e530a1db5cde92dba9798ae
LukeT2340 <lukethompson220@gmail.com>
2024-05-19 20:08:32 +1000

    Added footer, fixed other formatting.

--- frontend/src/sharedComponents/BlogPreview.js
+++ frontend/src/sharedComponents/BlogPreview.js
@@ -9,14 +9,14 @@ const BlogPreview = (blog) => {
     const formattedDate = createdAt.toISOString().split('T')[0];
     const tags = blog.tags ? blog.tags.split(', ') : [];
     return (
-        <div className={`container d-flex shadow flex-column col-md-3 col-sm-10 p-0 m-2 ${styles.blogPreview}`}>
-            <Link to={`/${urlTitle}`} className={`${styles.titleLink}`}>
+        <div className={`d-flex flex-column shadow col-md-2 col-sm-10 p-0 m-2 ${styles.blogPreview}`}>
+            <Link to={`/${urlTitle}`} className={`d-flex ${styles.titleLink} flex-column justify-items-center`}>
                 <img src={blog.thumbnail} className={`${styles.thumbnail} mb-2`}></img>
                 <h1 className={`${styles.title} mx-3`}>{blog.title}</h1>
             </Link>
             <p className={`${styles.description} mx-3`}>{blog.description}</p>
             <div className="m-2 mt-auto">
-                <div className="d-flex mt-auto m-2">
+                <div className="d-flex ">
                     {tags.map((tag, index) => ( 
                         <Link to={`/tag/${tag}`} className={`${styles.tagLink}`} key={index}>
                             <p className={`${styles.tagName}`}>{tag}</p>
