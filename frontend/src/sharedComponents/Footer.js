f728b8d61233db10817979af44a2af0d9c9e5af9
LukeT2340 <lukethompson220@gmail.com>
2024-05-20 20:48:31 +1000

    Adjuted footer

--- frontend/src/sharedComponents/Footer.js
+++ frontend/src/sharedComponents/Footer.js
@@ -1,28 +1,29 @@
 import React from "react";
 import styles from "./Footer.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faQ } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.container}>
                 <div className="row">
-                    <div className={`${styles.about} col-md-4`}>
+                    <div className={`${styles.about} col-md-6`}>
                         <h5>About</h5>
                         <p className={`${styles.paragraph}`}>Welcome to Master Swift! This blog is dedicated to providing high-quality tutorials, tips, and resources for learning SwiftUI - Apple's powerful and intuitive framework for building beautiful and engaging user interfaces across all Apple platforms.</p>
                     </div>
-                    <div className={`${styles.contact} col-md-4`}>
+                    <div className={`${styles.contact} col-md-6`}>
                         <h5>Contact</h5>
-                        <p>Email: lukethompson220.com</p>
-                        <p>LinkedIn: <a>Luke Thompson</a></p>
-                        {/* Add more contact details as needed */}
-                    </div>
-                    <div className="col-md-4">
                         <a href="https://github.com/LukeT2340" className={styles.link}>
                             <img className={styles.linkIcon} src="./github-mark-white.png"></img>
                             <p>LukeT2340</p>
                         </a>
+                        <div className={`${styles.link} mt-2`}>
+                            <FontAwesomeIcon icon={faEnvelope} className={styles.linkIcon}/>
+                            <p>Email: lukethompson220.com</p>
+                        </div>
                     </div>
+
                 </div>
                 <hr className={styles.hr} />
                 <div className="row">
