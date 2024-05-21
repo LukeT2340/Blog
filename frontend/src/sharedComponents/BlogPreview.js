b6148e625a5df98af460026fc8729bb13a6129df
LukeT2340 <lukethompson220@gmail.com>
2024-05-21 19:44:59 +1000

    Altered css and boostrap for blog previews

--- frontend/src/sharedComponents/BlogPreview.js
+++ frontend/src/sharedComponents/BlogPreview.js
@@ -23,7 +23,7 @@ const BlogPreview = (blog) => {
 
     return (
         <div
-        className={`col-xl-2 col-lg-3 col-md-5 col-sm-10`}
+        className={`col-xl-2 col-lg-3 col-md-5 col-sm-10 mx-5 `}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
     >
